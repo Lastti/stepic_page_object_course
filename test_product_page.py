1c5e35cef3c97d497fe6d17dbceeb85608c577ab
ann <anya.keyn@gmail.com>
2022-01-18 14:42:38 +0300

    added parametrize and xfail tags for test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,27 @@
 import time
+import pytest
 
 from .pages import product_page
 from .pages.product_page import ProductPage
 from .pages.locators import ProductPageLocators
 from selenium.webdriver.common.by import By
 
-def test_guest_can_add_product_to_basket(browser):
+link_list = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+             pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
+
+
+@pytest.mark.parametrize('link', link_list)
+def test_guest_can_add_product_to_basket(browser, link):
     # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_cart_button()
@@ -21,4 +35,3 @@ def test_guest_can_add_product_to_basket(browser):
 
     # page.should_be_expected_name()
     # page.should_be_expected_price()
-
