724fef3b17aceaed99515ae969c51ca70bff739d
ann <anya.keyn@gmail.com>
2022-01-18 14:18:38 +0300

    added get name, get price functions; changed should_be_expected_name, should_be_expected_price

--- pages/product_page.py
+++ pages/product_page.py
@@ -14,9 +14,28 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_ADDED_MESSAGE), \
             "There is no message that product successfully added to basket"
 
+    def get_the_product_name(self):
+        name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
+        return name.text
+
+    def get_the_product_price(self):
+        price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        return price.text
+
+
+    def should_be_expected_name_vol2(self, name):
+        actual_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_FROM_BASKET)
+        assert actual_name.text == name, \
+            f'Product name from the basket: {actual_name.text} is not the same as actual product name: {name}'
+
+    def should_be_expected_price_vol2(self, price):
+        actual_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_FROM_BASKET)
+        assert actual_price.text == price, \
+            f'Price from the basket: {actual_price.text} is not the same as actual product price: {price}'
+
 
     def should_be_expected_name(self):
-        actual_name = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_FROM_BASKET)
+        actual_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_FROM_BASKET)
         expected_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME)
         assert actual_name.text == expected_name.text, \
             f'Product name from the basket: {actual_name.text} is not the same as actual product name: {expected_name.text}'
--- test_product_page.py
+++ test_product_page.py
@@ -6,13 +6,19 @@ from .pages.locators import ProductPageLocators
 from selenium.webdriver.common.by import By
 
 def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_cart_button()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    time.sleep(3000)
-    page.should_be_expected_name()
-    page.should_be_expected_price()
+    # time.sleep(3000)
+    price = page.get_the_product_price()
+    page.should_be_expected_price_vol2(price)
+    name = page.get_the_product_name()
+    page.should_be_expected_name_vol2(name)
+
+    # page.should_be_expected_name()
+    # page.should_be_expected_price()
 
